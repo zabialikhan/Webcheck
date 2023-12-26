abc5bb411d658a46e0000db9f88ff6f7b7960426
zabialikhan <154892885+zabialikhan@users.noreply.github.com>
2023-12-26 17:35:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// JavaScript code for client-side validation and interaction
+
+// Example code for handling login form submission
+function login() {
+    // Retrieve form data
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Perform validation and authentication
+    // Use appropriate AJAX requests to send data to the server and handle responses
+}
+
+// Example code for handling registration form submission
+function register() {
+    // Retrieve form data
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+
+    // Perform validation
+    // Use appropriate AJAX requests to send data to the server and handle responses
+}
+
+// Example code for adding a new car
+function addCar() {
+    // Retrieve form data
+    var make = document.getElementById('make').value;
+    var model = document.getElementById('model').value;
+    var color = document.getElementById('color').value;
+    var rentalPrice = document.getElementById('rentalPrice').value;
+
+    // Perform validation
+    // Use appropriate AJAX requests to send data to the server and handle responses
+}
